9f5b6ab774a5347ce4d0615e3c9181a8cb0caa02
Mekan777-alt <mekan2678@gmail.com>
2024-03-16 16:10:56 +0200

    added service to parse

---
+++ service/parse.py
@@ -0,0 +1,40 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+driver.get("https://bankrot.fedresurs.ru")
+
+try:
+    cookie_disclaimer = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-accept")))
+    cookie_disclaimer.click()
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "input.ng-untouched.ng-valid.ng-dirty")))
+
+    element.send_keys("Иванов Петр Иванович")
+    time.sleep(3)
+    element.send_keys(Keys.ENTER)
+    time.sleep(5)
+    card_elements = WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".u-card-result__wrapper")))
+
+    for card in card_elements:
+        card.screenshot('card.png')
+        time.sleep(3)
+        card.find_element(By.CSS_SELECTOR, ".u-svg-arr-to-right").click()
+
+        driver.switch_to.window(driver.window_handles[-1])
+        time.sleep(5)
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+except Exception as e:
+    print(e)
+finally:
+
+    driver.close()
+    driver.quit()
