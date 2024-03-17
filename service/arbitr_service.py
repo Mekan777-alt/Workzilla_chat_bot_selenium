8e550e213d7686bff774cf8568bcf23ed7b6c524
Daniil Ivanov <ivanovda010101@gmail.com>
2024-03-17 15:11:26 +0300

    MAIN: added ENTER button to arbitr_service

--- service/arbitr_service.py
+++ service/arbitr_service.py
@@ -2,11 +2,27 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Chrome()
+block_ip = 'ip block'
+
+chrome_options = Options()
+chrome_options.add_argument('--start-maximized')
+# chrome_options.add_argument('--window-position=0,-1500')
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument(
+    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
+chrome_options.add_argument("--log-level=3")
+# ob = Screenshot.Screenshot()
+
+driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome()
+
+# driver.set_window_size(1920, 1080)
 
 driver.get("https://kad.arbitr.ru/")
 
@@ -15,19 +31,20 @@ try:
     #     EC.presence_of_element_located((By.ID, "sug-participants")))
     # cookie_disclaimer.click()
 
-    element = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "#sug-participants > div > textarea")))
-
+    element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#sug-participants textarea'))
+        )
     element.send_keys("иванова евгения александровна")
     element.send_keys(Keys.ENTER)
 
-    button = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "button[alt='Найти'][type='submit']")))
-
-
-    button.click()
-
     time.sleep(3)
+
+    error_message_element = driver.find_elements(By.CSS_SELECTOR, '.b-page-message_error')
+
+    if error_message_element:
+        print(f'При запросе: https://kad.arbitr.ru/ произошла блокировка IP')
+
+    time.sleep(5)
 
     table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "b-cases")))
 
