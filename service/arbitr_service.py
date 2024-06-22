e73246d9baed29f2d6eb91f4b0c592f67e15ed74
root <root@vbqfrahhtd.local>
2024-06-22 10:02:30 +0000

    testing bot

--- service/arbitr_service.py
+++ service/arbitr_service.py
@@ -17,7 +17,7 @@ def check_table(fio):
     chrome_options = Options()
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument('--start-maximized')
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-setuid-sandbox")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
@@ -26,7 +26,7 @@ def check_table(fio):
         'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
     chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
     chrome_options.add_argument("--log-level=3")
-    chrome_driver_path = "/home/mekan/Рабочий стол/Workzilla_chat_bot_selenium/chromedriver"
+    chrome_driver_path = "/root/Workzilla_chat_bot_selenium/chromedriver"
     service = Service(executable_path=chrome_driver_path)
     driver = webdriver.Chrome(options=chrome_options, service=service)
 
